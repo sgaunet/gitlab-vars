9f712b70ca2b5ead94552ad00f527fb80fbd8c3c
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2023-04-29 15:40:13 +0200

    add tests

--- internal/gitlabapi/gitlabvars_test.go
+++ internal/gitlabapi/gitlabvars_test.go
@@ -211,3 +211,133 @@ func TestGetIndexOfVar(t *testing.T) {
 		})
 	}
 }
+
+func TestGetVarOfScope(t *testing.T) {
+	v :=
+		Variables{
+			{Key: "key1", Value: "value1", EnvironmentScope: "preprod/*"},
+			{Key: "key2", Value: "value2", EnvironmentScope: "preprod/*"},
+			{Key: "key3", Value: "value3", EnvironmentScope: "prod/*"},
+			{Key: "key3", Value: "value4", EnvironmentScope: ""},
+			{Key: "key3", Value: "value5", EnvironmentScope: "env"},
+		}
+
+	type args struct {
+		key   string
+		scope string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    Variable
+		wantErr bool
+	}{
+		{
+			name: "No variable",
+			args: args{
+				key:   "key4",
+				scope: "preprod",
+			},
+			want:    Variable{},
+			wantErr: true,
+		},
+		{
+			name: "key1",
+			args: args{
+				key:   "key1",
+				scope: "preprod/env1",
+			},
+			want: Variable{
+				Key:              "key1",
+				Value:            "value1",
+				EnvironmentScope: "preprod/*",
+			},
+			wantErr: false,
+		},
+		{
+			name: "key3",
+			args: args{
+				key:   "key3",
+				scope: "prod/env1",
+			},
+			want: Variable{
+				Key:              "key3",
+				Value:            "value3",
+				EnvironmentScope: "prod/*",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := v.GetVarOfScope(tt.args.key, tt.args.scope)
+			if got != tt.want {
+				t.Errorf("%s - v.GetVarOfScope() = %v, want %v", tt.name, got, tt.want)
+			}
+			isTestInError := gotErr != nil
+			if isTestInError != tt.wantErr {
+				t.Errorf("%s - v.GetVarOfScope() error = %v, want %v", tt.name, isTestInError, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestIsVarPartOfScope(t *testing.T) {
+	type args struct {
+		environment string
+		varScope    string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "env preprod scope preprod/*",
+			args: args{
+				environment: "preprod",
+				varScope:    "preprod/*",
+			},
+			want: false,
+		},
+		{
+			name: "env preprod scope preprod",
+			args: args{
+				environment: "preprod",
+				varScope:    "preprod",
+			},
+			want: true,
+		},
+		{
+			name: "env preprod scope preprod/*",
+			args: args{
+				environment: "preprod/env1",
+				varScope:    "preprod/*",
+			},
+			want: true,
+		},
+		{
+			name: "env empty scope empty",
+			args: args{
+				environment: "",
+				varScope:    "",
+			},
+			want: true,
+		},
+		{
+			name: "env * scope **",
+			args: args{
+				environment: "",
+				varScope:    "*",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsVarPartOfScope(tt.args.environment, tt.args.varScope); got != tt.want {
+				t.Errorf("IsVarPartOfScope() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
